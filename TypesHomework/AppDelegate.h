aad0f0df78b756b388bc6c400571c98397bc5050
riko105 <stepanpaholyk@gmail.com>
2016-06-19 20:54:44 +0300

    Completed homework

---
+++ TypesHomework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TypesHomework
+//
+//  Created by Stepan Paholyk on 4/13/16.
+//  Copyright © 2016 Stepan Paholyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TypesHomework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TypesHomework
+//
+//  Created by Stepan Paholyk on 4/13/16.
+//  Copyright © 2016 Stepan Paholyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
